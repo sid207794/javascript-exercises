49bf6d5540620e7c232e283bf19ac450c9ad2814
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-02-24 12:56:42 +0000

    Add if-else and for loop to join string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let conc_string = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (i=0; i<num; i++) {
+            conc_string += string;
+        }
+        return conc_string;
+    }
 };
 
 // Do not edit below this line
