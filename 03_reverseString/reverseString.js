3d2981f09bfaf5723e9b511093f2bdac7bcd094f
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-02-24 14:18:07 +0000

    Add for loop to create array of string characters and return them reversed and joined

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const rev_string = [];
+    for (i=0; i<string.length; i++) {
+        rev_string[i] = string[i];
+    }
+    rev_string.reverse();
+    return rev_string.join("");
 };
 
 // Do not edit below this line
