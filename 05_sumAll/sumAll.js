3a1f35f5dc5339aaa4250f211666c93d17394cfb
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-02-25 06:30:57 +0000

    Add if and for loop for summation of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1>0 && num2>0) {
+        if (num1 > num2) {
+            const temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
 
+        for (i=num1; i<=num2; i++) {
+            sum += i;
+        }
+        
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
