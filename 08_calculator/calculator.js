e5bb9a14550b626e1779b9cca3b81287e77b4909
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-02 16:31:16 +0000

    Add solution to js and remove skip from spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, initial) => total+initial, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, initial) => total*initial);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  } else if (num > 1) {
+    product = 1;
+    for (i=num; i>0; i--) {
+      product *= i;
+    }
+    return product;
+  }
 };
 
 // Do not edit below this line
