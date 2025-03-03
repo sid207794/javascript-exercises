44033517fe067f7d17e46e6393da2f31d5cde1b0
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-03 12:21:50 +0000

    Add code to function in .js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const year = new Date().getFullYear();
+    const arr = array.map((obj) => {
+        if (obj.yearOfDeath === undefined) {
+            return ({name: obj.name, age: +year-obj.yearOfBirth,});
+        } else {
+            return ({name: obj.name, age: obj.yearOfDeath-obj.yearOfBirth,});
+        }
+    }).sort((a, b) => {return b.age-a.age;});
 
+    return arr[0];
 };
 
 // Do not edit below this line
