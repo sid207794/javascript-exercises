ebdbbd36c0bf9f7bb94568587f0f3757d8cb99fc
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-03 08:12:33 +0000

    Add code to function in .js and remove skip from tests in .spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0, 1];
+    if (+num >= 0) {
+        for (i=2; i<=num; i++) {
+            fib[i] = fib[i-1] + fib[i-2]
+        }
 
+        return fib[num];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
