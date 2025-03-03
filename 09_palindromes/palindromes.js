7b0a5f49ed9254661837d7f96bfe9b9d8ffe9da7
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-03 06:37:07 +0000

    Add code to function in .js and remove skip from tests in .spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arr  = string.toLowerCase().split("")
+    for (i=0; i<arr.length; i++) {
+        if (arr[i] === " " || arr[i] === "," || arr[i] === "." || arr[i] === "!") {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
 
+    return arr.join("") === arr.reverse().join("");
 };
 
 // Do not edit below this line
